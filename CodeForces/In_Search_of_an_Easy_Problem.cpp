2b79134f29c2a7269523ddad953a6bb786df1f3b
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-05-14 22:06:12 +0530

    CP 10

--- CodeForces/In_Search_of_an_Easy_Problem.cpp
+++ CodeForces/In_Search_of_an_Easy_Problem.cpp
@@ -3,4 +3,21 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int flag = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 1)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag)
+        cout << "HARD";
+    else
+        cout << "EASY";
 }
