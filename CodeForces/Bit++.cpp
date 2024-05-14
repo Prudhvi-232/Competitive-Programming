1585a60b47bedb79a92eb63ca3f2a98ab793c483
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-05-14 18:14:26 +0530

    done Bit++ sum

--- CodeForces/Bit++.cpp
+++ CodeForces/Bit++.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+    int y = 0;
+    while (t--)
+    {
+        string x;
+        cin >> x;
+        if (x == "++X")
+        {
+            ++y;
+        }
+        else if (x == "X++")
+        {
+            y++;
+        }
+        else if (x == "X--")
+        {
+            y--;
+        }
+        else if (x == "--X")
+        {
+            --y;
+        }
+    }
+    cout << y << endl;
+}
