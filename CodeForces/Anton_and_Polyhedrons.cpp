ffc2bb42aacdd26655c24be34427cb1338c66d23
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-05-14 21:40:12 +0530

    CP 8

--- CodeForces/Anton_and_Polyhedrons.cpp
+++ CodeForces/Anton_and_Polyhedrons.cpp
@@ -3,4 +3,23 @@ using namespace std;
 
 int main()
 {
+    long long n;
+    cin >> n;
+    long long sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        if (s == "Tetrahedron")
+            sum = sum + 4;
+        else if (s == "Cube")
+            sum = sum + 6;
+        else if (s == "Octahedron")
+            sum = sum + 8;
+        else if (s == "Dodecahedron")
+            sum = sum + 12;
+        else if (s == "Icosahedron")
+            sum = sum + 20;
+    }
+    cout << sum;
 }
