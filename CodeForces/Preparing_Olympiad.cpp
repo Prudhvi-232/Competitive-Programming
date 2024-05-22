c985c465e9d12acb1351cd902176479ae8fe36f2
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-05-23 00:00:20 +0530

    done for today

---
+++ CodeForces/Preparing_Olympiad.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, l, r, x;
+    cin >> n >> l >> r >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
