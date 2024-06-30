d43362bbadf3e7c6a5dd51ef4bc44332842f5794
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-06-30 14:36:44 +0530

    Refactor main function for better readability and performance

--- CodeChef/boiler.cpp
+++ CodeChef/boiler.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
-using namespace std;
+#define int long long
 
-int main()
+using namespace std;
+void solve()
 {
+    int n;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
