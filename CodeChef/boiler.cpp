43b2d5b4799c37243d0c744fdd5ca43d5f14bbe6
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-05 10:47:16 +0530

    chore: Update boiler.cpp to improve code readability and performance

--- CodeChef/boiler.cpp
+++ CodeChef/boiler.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define int long long
+#define e endl;
 
 using namespace std;
 void solve()
@@ -11,7 +12,8 @@ int32_t main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
+    cout.tie(NULL);
+    int t=1;
     cin >> t;
     while (t--)
     {
