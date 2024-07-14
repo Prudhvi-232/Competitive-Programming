1a8d7912e68e39178d4b7af7207abd454eb268ce
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-14 20:01:05 +0530

    feat: Add boiler code for solving a problem

---
+++ boiler.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+#define e endl;
+#define yes cout<<"YES\n"<<endl;
+#define no cout<<"NO\n"<<endl;
+
+using namespace std;
+void solve()
+{
+    int n;cin>>n;
+    
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t=1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
