bbf823bc522cb7f7dba2a18b1ea29b6b18e5e6fb
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-19 21:46:34 +0530

    Refactor Array_Cloning_Technique.cpp for improved code readability and performance

--- CodeForces/Array_Cloning_Technique.cpp
+++ CodeForces/Array_Cloning_Technique.cpp
@@ -11,9 +11,37 @@ void solve()
     int n;
     cin >> n;
     vi a(n + 1);
+    unordered_map<int, int> mp;
     fr(n)
     {
         cin >> a[i];
+        mp[a[i]]++;
+    }
+    int maxelement = 0;
+    for (auto x : mp)
+    {
+        maxelement = max(maxelement, x.second);
+    }
+    int ct = 0;
+    if (maxelement == n)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        int repe = maxelement;
+        int m = n - maxelement;
+        while (repe < n)
+        {
+            ct = ct + 1 + min(repe, m);
+            m = m - repe;
+            repe = repe * 2;
+            if (m < 0)
+            {
+                break;
+            }
+        }
+        cout << ct << endl;
     }
 }
 int32_t main()
