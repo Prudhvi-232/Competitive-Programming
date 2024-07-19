ac039a39fbe282051577f3104242f4201855c840
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-19 21:46:48 +0530

    Refactor Make_AP.cpp for improved code readability and performance

--- CodeForces/Make_AP.cpp
+++ CodeForces/Make_AP.cpp
@@ -13,27 +13,69 @@ void solve()
         yes return;
     }
     else{
-        if(a<b && a<c){
+        if(a<=b || a<=c){
             if(b>c){
                 int d = b - c;
-                if(d%a==0){
-                    yes
-                }
-                else{
-                    no
+                if((b+d)%a==0 && b+d>a){
+                    yes return;
                 }
             }
             if(b<c){
                 int d = c-b;
-                if(d%a==0){
-                    yes
+                if((b-d)%a==0 && b-d>a){
+                    yes return;
                 }
-                else{
-                    no
+            }
+            if(b==c){
+                if(b%a==0 && b>a){
+                    yes return;
+                }
+            }
+        }
+        if(b<=a || b<=c){
+            if(a==c){
+                if(a%b==0 && a>b){
+                    yes return;
+                }
+            }
+            if(a>c){
+                if((a-c)%2==0){
+                    int d = (a-c)/2;
+                    if((c+d)%b==0 && c+d>b){
+                    yes return;
+                    }
+                }
+            }
+            if(a<c){
+                if((c-a)%2==0){int d = (c-a)/2;
+                
+                if((c-d)%b==0 && c-d>b){
+                    yes return;
+                }}
+            }
+
+        }
+        if(c<=a || c<=b){
+            if(a==b){
+                if(a%c==0 && a>c){
+                    yes return;
+                }
+            }
+            if(a>b){
+                int d = (a-b);
+                if((b-d)%c==0 && b-d>c){
+                    yes return;
+                }
+            }
+            if(a<b){
+                int d = (b-a);
+                if((b+d)%c==0 && b+d>c){
+                    yes return;
                 }
             }
         }
     }
+    no return;
 }
 int32_t main()
 {
