be79b4f1e4545d26b3f1ae83e261cd12295c009e
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-20 18:39:53 +0530

    feat: Refactor Mocha_and_Math.cpp for improved code readability and performance

--- CodeForces/Mocha_and_Math.cpp
+++ CodeForces/Mocha_and_Math.cpp
@@ -1,16 +1,34 @@
 #include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
 using namespace std;
-
-int main()
+void solve()
 {
-    int t;cin>>t;
-    while(t--){
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n;i++){
-            cin >> a[i];
-        }
-        
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    long long bit =pow(2,32)-1ll ;
+    fr(n)
+    {
+        long long x;
+        cin >> x;
+        bit = bit & x;
     }
+    cout << bit << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
