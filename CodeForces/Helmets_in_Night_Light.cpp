1e228134c04e3947412153173a000e2e8bb42c25
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-21 22:47:50 +0530

    feat: Add Helmets_in_Night_Light.cpp for implementing a solution to a specific problem

---
+++ CodeForces/Helmets_in_Night_Light.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n,p;
+    cin >> n>>p;
+    int a[n], b[n];
+    fr(n){
+        cin >> a[i];
+    }
+    fr(n){
+        cin >> b[i];
+    }
+    
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
