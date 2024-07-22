58fb02a4bef38c8820e2dd0a45d25fe90c389f28
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-22 22:35:19 +0530

    feat: Add Mangoes.cpp for implementing a solution to a specific problem

---
+++ HackerEarth/Mangoes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
