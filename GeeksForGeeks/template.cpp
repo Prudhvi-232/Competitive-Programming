5d745884e3f5b9ce352efc8aec87800de67dbd01
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-22 22:35:08 +0530

    feat: Add template.cpp for implementing a solution template in GeeksForGeeks

---
+++ GeeksForGeeks/template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+    }
+};
+
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        solution ob;
+        cout << ob.reverseWord(s) << endl;
+    }
+    return 0;
+}
