eae2f3b1a94ff8476bd67e0996e67059f6772285
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-27 00:33:53 +0530

    feat: Add Fun.cpp for implementing a solution to a fun problem

---
+++ CodeForces/Fun.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    int a = 1, b = 1, c = 1;
+    int count = 0;
+    
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
