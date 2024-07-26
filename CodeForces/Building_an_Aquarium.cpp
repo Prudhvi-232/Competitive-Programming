b534e049ecd9c82a48886ee04cbe044fed0cccef
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-27 00:32:42 +0530

    feat: Add Building_an_Aquarium.cpp for implementing a solution to build an aquarium with given specifications

---
+++ CodeForces/Building_an_Aquarium.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define vl vector<long long>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n,x;
+    cin >> n>>x;
+    vl a(n);
+    for(n) cin >> a[i];
+    
+
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
