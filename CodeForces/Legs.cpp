29aa823eafa927f95c807d6ce5450fc793d4007d
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-27 00:34:01 +0530

    feat: Add Legs.cpp for implementing a solution to calculate the number of legs in a given number of animals

---
+++ CodeForces/Legs.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    int x = n / 4;
+    int y = n % 4;
+    int z = y / 2;
+    cout << x + z << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
