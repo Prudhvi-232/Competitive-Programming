6689c9cc0240ef048026b3adef3f65f35e2e9cf8
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-28 01:32:52 +0530

    feat: Add Collecting_Game.cpp for implementing a solution to collect game elements

---
+++ CodeForces/Collecting_Game.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define vl vector<long long>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    fr(n) cin >> a[i];
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
