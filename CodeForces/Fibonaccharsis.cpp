f9b74dcff71d2031332599e90d63540bafa6e11f
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-28 01:33:14 +0530

    feat: Add Fibonaccharsis.cpp for implementing a solution to a Fibonacci sequence problem

---
+++ CodeForces/Fibonaccharsis.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define vl vector<long long>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    ll n,k;
+    cin >> n>>k;
+    
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
