c085e001312dbffce197f3ca5620a14b13121422
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-07-31 22:43:40 +0530

    feat: Add Jumping_Through_Segments.cpp for implementing a solution to a CodeForces problem

---
+++ CodeForces/Jumping_Through_Segments.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define vi vector<int>
+#define vl vector<long long>
+#define fr(n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        int l,r;cin>>l>>r;
+        
+    }
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
