6d3bbe0618f31fd6fdf68b49e5e12f17a74c6219
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-08-05 21:23:20 +0530

    feat: Implement efficient algorithm to check for T-prime numbers in T_primes.cpp

--- CodeForces/T_primes.cpp
+++ CodeForces/T_primes.cpp
@@ -1,50 +1,60 @@
 #include <bits/stdc++.h>
-#define yes cout << "YES" << endl;
-#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 #define vi vector<int>
 #define vl vector<long long>
 #define fr(n) for (int i = 0; i < n; i++)
 typedef long long ll;
 using namespace std;
 
-void solve() {
+bool isPrime(ll n) {
+    if (n <= 1) return false; // Numbers less than or equal to 1 are not prime
+    if (n <= 3) return true; // 2 and 3 are prime numbers
+    if (n % 2 == 0 || n % 3 == 0) return false; // Eliminate multiples of 2 and 3
+
+    for (ll i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false; // Check divisibility
+    }
+
+    return true; // If no factors found, n is prime
+}
+void solve()
+{
     ll n;
     cin >> n;
-    
-    if (n <= 1) {
-        no; // 0 and 1 are not prime numbers
+    ll ct = 0;
+    ll ab = sqrt(n);
+    if (n <= 5 && n != 4)
+    {
+        no;
         return;
     }
-    
-    int flag = -2;
-    for (ll i = 2; i*i<= n*2; i++) {
-        if (flag == 0 && n % i == 0 ) {
-            flag = -1;
-            break;
-        }
-        if (n % i == 0 && flag == -2) {
-            flag = 0;
-        }
+    if (ab * ab != n)
+    {
+        no;
+        return;
     }
-    
-    if (flag == -1) {
-        no
-    } else if (flag == -2) {
-        no // prime number
-    } else if (flag == 0) {
-        yes // first divisor found
-    } else {
-        yes
+    else
+    {
+        if (isPrime(ab))
+        {
+            yes;
+            return;
+        }
+        no;
+        return;
     }
 }
 
-int32_t main() {
+int32_t main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
