e06ed646a0931e8b2620bf8b67186ff760ece792
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-08-07 23:50:14 +0530

    feat: Update Prefix_Flip.cpp for improved performance and readability

--- CodeForces/Prefix_Flip.cpp
+++ CodeForces/Prefix_Flip.cpp
@@ -1,37 +1,36 @@
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update> super_set;
+// to run these use wsl g++ -o name file.cpp and to print output ./name
+// #define rank order_of_key
+// #define atrank find_by_order
 #define yes cout << "YES" << endl;
 #define no cout << "NO" << endl;
 #define vi vector<int>
 #define vl vector<long long>
-#define fr(n) for (int i = 0; i < n; i++)
+#define pb push_back
+#define fr(n) for (long long i = 0; i < n; i++)
+#define fv(i,a) for(auto i:a)cout<<i<<" "; 
+#define sort(a) sort(a.begin(), a.end());
+#define reverse(a) reverse(a.begin(), a.end());
+#define mup unordered_map<long long,long long>
+#define mp unordered_map<long long,long long>
 typedef long long ll;
 using namespace std;
 void solve()
 {
     ll n;
     cin >> n;
-    string a, b;
-    cin >> a >> b;
-    int x = 0;
-    fr(n){
-        if(a[n-i-1] != b[n-i-1]){
-            x++;
-            if(a[0]==b[n-i-1]){
-                cout << 1 << " " << n - i<<" ";
-            }
-            else{
-                cout << n - i << " ";
-            }
-        }
-    }
-    cout <<" "<< endl;
 }
 int32_t main()
 {
+//typedef gp_hash_table<int, int> hash_table;
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t = 1;
+    ll t = 1;
     cin >> t;
     while (t--)
     {
